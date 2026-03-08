5cd0170d47492c5777c60a26ead8c4404afd85a2
b-u-g-g <tsakshi1708@gmail.com>
2026-03-08 23:55:13 +0530

    terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-state-rg"
-    storage_account_name = "tfstatefinancerag"
-    container_name       = "tfstate"
-    key                  = "finance-rag-llm.tfstate"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
   }
 }
