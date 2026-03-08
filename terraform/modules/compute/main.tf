ebf0c0c9a39e74f82b5ebf8dc6e646759e920176
b-u-g-g <tsakshi1708@gmail.com>
2026-03-09 00:33:43 +0530

    ...

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -3,6 +3,7 @@ resource "azurerm_public_ip" "pip" {
   resource_group_name = var.resource_group_name
   location            = var.location
   allocation_method   = "Static"
+  sku                 = "Standard"
   tags                = var.tags
 }
 
