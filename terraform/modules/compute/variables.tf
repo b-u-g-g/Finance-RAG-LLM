e0776795263df2ac9e6405a018af1cecb53e891e
bugg <147278023+b-u-g-g@users.noreply.github.com>
2026-03-19 15:02:56 +0530

    Delete terraform directory

--- terraform/modules/compute/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-variable "vm_name"             { type = string }
-variable "vm_size"             { type = string }
-variable "admin_username"      { type = string }
-variable "ssh_public_key"      { type = string }
-variable "subnet_id"           { type = string }
-variable "nsg_id"              { type = string }
-variable "tags"                { type = map(string) }
--- terraform/modules/security/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-variable "nsg_name"            { type = string }
-variable "subnet_id"           { type = string }
-variable "tags"                { type = map(string) }
